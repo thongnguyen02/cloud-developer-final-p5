14381c6b50037ef64f7ff00d7f73569f6d0e3ee4
THONG <thongnguyen02@gmail.com>
2022-11-21 20:03:19 +0700

    update file

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'veunhp76ha'
+const apiId = 'wwnxe3ig3l'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
